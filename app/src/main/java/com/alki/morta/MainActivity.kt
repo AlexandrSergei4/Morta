d8c5b14232be8d9c745b46ccb2ba062f19560aed
Кириллов Александр Сергеевич <ASKirillov1@sberbank.ru>
2021-05-16 19:56:26 +0400

    правка предупреждения о работе в оффлайн

--- app/src/main/java/com/alki/morta/MainActivity.kt
+++ app/src/main/java/com/alki/morta/MainActivity.kt
@@ -1,23 +1,23 @@
 package com.alki.morta
 
 import android.content.Context
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
+import android.net.NetworkInfo
 import android.os.Bundle
-import android.util.Log
 import android.view.MenuItem
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.drawerlayout.widget.DrawerLayout
-import androidx.lifecycle.viewModelScope
 import androidx.navigation.NavController
-import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
-import androidx.navigation.ui.*
+import androidx.navigation.ui.NavigationUI
 import com.alki.morta.databinding.ActivityMainBinding
 import com.alki.morta.domain.AppRepository
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
-import timber.log.Timber
 
 class MainActivity : AppCompatActivity() {
 
@@ -26,12 +26,29 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
+        val isConnected: Boolean
+        val cm = application.applicationContext.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
         val repository = AppRepository(application.applicationContext)
+        if (android.os.Build.VERSION.SDK_INT < 23) {
+            val activeNetwork: NetworkInfo? = cm.activeNetworkInfo
+            isConnected = activeNetwork?.isConnectedOrConnecting == true
+        } else {
+            var capabilities = cm.getNetworkCapabilities(cm.activeNetwork)
+            isConnected = capabilities != null
+                       && capabilities.hasCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET)
+                       && capabilities.hasCapability(NetworkCapabilities.NET_CAPABILITY_VALIDATED)
 
-        CoroutineScope(Dispatchers.Default).launch {
-            repository.refresh()
         }
+
+        if (isConnected)
+            CoroutineScope(Dispatchers.Default).launch { repository.refresh()}
+        else
+            Toast.makeText(
+                application.applicationContext,
+                "Отсутствует подключение к интернету, приложение работает в оффлайн режиме",
+                Toast.LENGTH_LONG
+            ).show()
+
         val binding =
             DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
         val navHostFragment =
